d120cc5802726e9def49475f6dcdcd730a2b76d6
Veronika <geras-veronika@rambler.ru>
2024-03-18 22:02:47 +0800

    Добавление класса Fraction для калькулятора дробей и создание теста для проверки введенных данных

---
+++ Laboratornay_3/Fraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornay_3
+{
+    public class Fraction
+    {
+        public int numerator;
+        public int denominator;
+
+        public Fraction(int num, int den)
+        {
+            if (den == 0)
+                throw new ArgumentException("Знаменатель не может быть равен 0");
+            this.numerator = num;
+            this.denominator = den;
+        }
+        //Метод, возвращающий строковое представление дроби в формате числитель/знаменатель
+        public string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
---
+++ Laboratornay_3Tests/FractionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Laboratornay_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornay_3.Tests
+{
+    [TestClass()]
+    public class FractionTests
+    {
+        [TestMethod()]
+       public void ToStringTest()
+        {
+            int num = 3;
+            int den = 4;
+            Fraction fraction = new Fraction(num, den);
+
+            string result = fraction.ToString();
+
+            Assert.AreEqual("3/4", result);
+        }
+
+    }
+}
